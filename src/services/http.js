3d75427dd0516b76421754ee7aeaa3495b4d11ca
José Carlos <jcolscript@gmail.com>
2020-03-23 23:30:52 -0300

    criando serviço do axios

---
+++ src/services/http.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const client = axios.create({
+  baseURL: 'https://api.github.com/users/',
+  timeout: 1000,
+  headers: { 'Content-Type': 'application/json' },
+});
+
+export default client;
