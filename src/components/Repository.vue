4aaa43b44c4a609ef3b7e188b3c1a7af99716e39
José Carlos <jcolscript@gmail.com>
2020-03-24 03:18:25 -0300

    refactor do layout

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -1,48 +1,52 @@
 <template>
   <div class="col-lg-4 col-md-6 col-12 mb-4">
-    <div class="card">
+    <div class="card h-100">
       <div class="card-body">
-        <div class="row">
-          <div class="col-md-8 col-6">
-            <h5 class="card-title">
-              <i class="fa fa-book"></i>
-              &nbsp;<a :href="repository.html_url" target="_blank">{{ repository.name }}</a>
-            </h5>
-          </div>
-          <div class="col-md-4 col-6 text-right">
-            <i class="fa fa-star text-primary"></i> {{ repository.stargazers_count }}
-          </div>
-        </div>
-
+        <h5 class="card-title">
+          <i class="fa fa-book text-primary"></i>
+          &nbsp;<a :href="repository.html_url" target="_blank">{{ repository.name }}</a>
+        </h5>
         <p class="card-text">
           {{ repository.description }}
         </p>
-        <p class="card-text">
-          <small class="text-muted">updated at {{ repository.updated_at | formatDate }}</small>
-        </p>
+      </div>
+      <div class="card-footer-pers d-flex justify-content-between">
+        <span class="text-left">
+          <i class="fa fa-code text-primary" v-if="repository.language"></i>
+          {{ repository.language }}
+        </span>
+        <span class="text-right">
+          <i class="fa fa-star text-primary"></i> {{ repository.stargazers_count }}
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import { format, parseISO } from 'date-fns';
-import pt from 'date-fns/locale/pt';
-
 export default {
   name: 'Repository',
   props: {
     repository: Object,
   },
-  filters: {
-    formatDate(value) {
-      let val = value;
-      if (!val) return '';
-      val = format(parseISO(val), 'dd/MM/yyyy', { locale: pt });
-      return val;
-    },
-  },
 };
 </script>
 
-<style lang="sass" scoped></style>
+<style lang="scss" scoped>
+.card-body {
+  .card-title {
+    font-size: 16px;
+  }
+
+  .card-text {
+    font-size: 14px;
+  }
+}
+
+.card-footer-pers {
+  padding: 0.75rem 1.25rem 1.25rem;
+  span {
+    font-size: 14px;
+  }
+}
+</style>
