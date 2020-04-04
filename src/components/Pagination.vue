0db90c29c193a39a1a07219c6d6b6a9a4792db43
José Carlos <jcolscript@gmail.com>
2020-04-03 23:02:42 -0300

    correcao no componente de paginacao

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -16,7 +16,7 @@
         >
           <a class="page-link pg" @click="navigate($event, pg)" href="#">{{ pg }}</a>
         </li>
-        <li class="page-item" :class="{ disabled: Math.round(this.repos / 15) + 1 }">
+        <li class="page-item" :class="{ disabled: page === Math.round(this.repos / 15) + 1 }">
           <a class="page-link" href="#" @click="navigate($event, page + 1)" aria-label="Next">
             <span aria-hidden="true">&raquo;</span>
             <span class="sr-only">Next</span>
